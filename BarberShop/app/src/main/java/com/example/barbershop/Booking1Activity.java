b5f5fc7d66571d8ed039ce83a6a75c58ab662094
Vanhieu <hieulehieu0@gmail.com>
2021-05-28 11:24:07 +0700

    1
    1

--- BarberShop/app/src/main/java/com/example/barbershop/Booking1Activity.java
+++ BarberShop/app/src/main/java/com/example/barbershop/Booking1Activity.java
@@ -1,6 +1,7 @@
 package com.example.barbershop;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -10,9 +11,10 @@ import java.util.ArrayList;
 
 public class Booking1Activity extends AppCompatActivity {
     RecyclerView revBooking1;
+    RecyclerView revBooking2;
     Booking1Adapter booking1Adapter;
     ArrayList<Booking1> list;
-
+    ArrayList<Booking1> list1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,5 +37,24 @@ public class Booking1Activity extends AppCompatActivity {
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 4);
         revBooking1.setLayoutManager(gridLayoutManager);
 
+
+        revBooking2 = findViewById(R.id.list2);
+
+        list1 = new ArrayList<>();
+
+        list1.add(new Booking1("Fri 30 Aug"));
+        list1.add(new Booking1("Fri 30 Aug"));
+        list1.add(new Booking1("Set 31 Aug"));
+        list1.add(new Booking1("Sun 1 Sep"));
+        list1.add(new Booking1("Mon 2 Sep"));
+        list1.add(new Booking1("Tue 3 Sep"));
+        list1.add(new Booking1("Wed 4 Sep"));
+
+        booking1Adapter = new Booking1Adapter(getApplicationContext(), list1);
+        revBooking2.setAdapter(booking1Adapter);
+        GridLayoutManager gridLayoutManager1= new GridLayoutManager(this, 4);
+        revBooking2.setLayoutManager(gridLayoutManager1);
+
     }
+
 }
